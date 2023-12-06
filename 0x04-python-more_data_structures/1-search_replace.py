e7f5bc4341e65369ae0414ebaf51618cafdab470
lagovic <lagovic@gmail.com>
2023-12-06 13:51:11 -0800

    indentation 0x04

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-search_replace = __import__('1-search_replace').search_replace
+def search_replace(my_list, search, replace):
+    n_list = list(my_list)
+    for i in range(len(n_list)):
+        if n_list[i] == search:
+            n_list[i] = replace
+            return n_list
